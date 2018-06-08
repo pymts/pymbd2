98be0579fd808622c190385e2b94b455180d0dc9
yamazakiopst <yamazaki.kouki@opst.co.jp>
2018-06-08 15:28:03 +0900

    token隠し

--- index.py
+++ index.py
@@ -24,4 +24,4 @@ async def on_message(message):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
 
-client.run('NDUzNzQ5Mjg5NzY0NzgyMDkw.DfuQbg.cqNwfR8pxY1k1RV3yKj6dMlfdy0')
+client.run(process.env.BOT_TOKEN)
